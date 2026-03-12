38d4a7f72cc242c024a21b3a65b3be4a94718815
verneque-dev <verneque.dev@gmail.com>
2026-03-11 21:07:05 -0300

    login com set cookies httponly, toast e router push desenvolvidos e proxy do dashboard funcionando

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,57 @@
+"use client"
+
 import { urlApi } from "@/lib/api"
-import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
+import Image from "next/image"
+import { toast } from "sonner"
 
 export default function Login() {
-  async function handleLogin(formData: FormData) {
-    "use server"
+  const router = useRouter()
+  async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
     const email = formData.get("email")
     const password = formData.get("senha")
 
-    const res = await fetch(`${urlApi}/api/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email,
-        password
+    async function login() {
+      const res = await fetch(`${urlApi}/api/login`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        })
       })
-    })
-    if (res.ok) {
-      redirect("/dashboard")
+      if (!res.ok) {
+        throw new Error("login inválido")
+      }
+      router.push("../dashboard")
     }
+    toast.promise(login(), {
+      success: "Login feito com sucesso.",
+      error: "Login inválido.",
+      loading: "Logando..."
+    })
   }
   return (
-    <form action={handleLogin} className="flex flex-col gap-5 bg-white w-100 h-100 m-20 mx-auto shadow-2xl rounded-lg">
-      <input type="text" name="email" placeholder="email" required className=""/>
-      <input type="text" name="senha" placeholder="senha" required/>
+    <form method="post" onSubmit={handleLogin} className="flex flex-col bg-black gap-5 h-100 m-15 mx-auto shadow-2xl rounded-2xl
+    p-10 items-center md:w-100">
+      <div className="h-28 w-28">
+        <Image
+          src="/logo.jpeg"
+          alt="King Imports"
+          width={500}
+          height={500}
+          className="h-full w-full"
+        />
+      </div>
+      <input type="text" name="email" placeholder="email" required className="bg-white h-10 p-4 rounded-lg w-64"/>
+      <input type="text" name="senha" placeholder="senha" required className="bg-white h-10 p-4 rounded-lg w-64"/>
 
-      <button type="submit"> logar </button>
+      <button type="submit" className="bg-yellow-300 text-white font-bold h-10 w-44 rounded-lg mt-auto"> logar </button>
     </form>
   )
 }
