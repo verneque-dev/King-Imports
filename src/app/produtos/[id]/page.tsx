fac518d39518c0c6d279dcefc694adaf18c999bf
verneque-dev <verneque.dev@gmail.com>
2026-04-02 13:46:57 -0300

    ajuste de responsividade na pagina de produto unico

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -50,7 +50,7 @@ export default async function ProdutoDetails(context: { params: Promise<{ id: st
 
   return (
     <div className="w-[90%] mx-auto p-1 md:p-8 rounded-lg my-8 shadow-[0_6px_24px_rgba(0,0,0,0.08)]">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-15 p-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-15 p-8">
         <div className="aspect-square">
           <CarroselProdutos produto={produto} />
         </div>
