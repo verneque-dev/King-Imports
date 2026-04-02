3d698a2154a4346b94634f0e6573b11a2e238190
verneque-dev <verneque.dev@gmail.com>
2026-04-02 13:50:56 -0300

    ajuste no skeleton loading da pagina de produto

--- src/app/produtos/[id]/loading.tsx
+++ src/app/produtos/[id]/loading.tsx
@@ -1,7 +1,7 @@
 export default function LoadingProduto() {
   return (
     <div className="w-[90%] mx-auto p-1 md:p-8 rounded-lg my-8 shadow-[0_6px_24px_rgba(0,0,0,0.08)]">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-15 p-8">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-15 p-8">
         <div className="aspect-square bg-gray-300 animate-pulse rounded-lg">
 
         </div>
